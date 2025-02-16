f7d7fd04fba0c88abd30c1502e7981b96bc0af77
Lint Action <lint-action@samuelmeuli.com>
2025-02-16 20:05:17 +0000

    Fix code style issues with Black

--- app/service.py
+++ app/service.py
@@ -15,6 +15,7 @@ try:
 except FileNotFoundError:
     raise RuntimeError(f"Model not found at {MODEL_PATH}.")
 
+
 # POST using a link to the CSV file as data
 @app.post("/score1")
 def score(data: str):
@@ -26,7 +27,6 @@ def score(data: str):
         raise HTTPException(status_code=400, detail=str(e))
 
 
-
 # POST using a CSV file as data
 @app.post("/score2")
 def score(file: UploadFile):
@@ -36,6 +36,7 @@ def score(file: UploadFile):
         return {"predictions": predictions.tolist()}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+
 
 if __name__ == "__main__":
     # Run the FastAPI app
